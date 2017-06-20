c66da98c21fb884bd8f3164b99011d34f0872f00
ferid.cafer <ferid.developer@gmail.com>
2017-06-20 14:10:52 +0300

    v4.1.0

--- app/src/main/java/com/ferid/app/frequentcontacts/widget/FrequentContactsWidget.java
+++ app/src/main/java/com/ferid/app/frequentcontacts/widget/FrequentContactsWidget.java
@@ -24,12 +24,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffXfermode;
-import android.graphics.Rect;
 import android.net.Uri;
 import android.util.Base64;
 import android.view.View;
@@ -157,7 +151,7 @@ public class FrequentContactsWidget extends AppWidgetProvider {
 
         remoteViews.setImageViewBitmap(
                 context.getResources().getIdentifier(
-                        componentId, null, context.getPackageName()), getCroppedBitmap(bitmap, 200));
+                        componentId, null, context.getPackageName()), bitmap);
     }
 
     /**
@@ -196,42 +190,4 @@ public class FrequentContactsWidget extends AppWidgetProvider {
                 componentId, null, context.getPackageName()), pendingIntent);
     }
 
-    /**
-     * Get rounded version of the given photo
-     * @param bmp
-     * @param radius
-     * @return
-     */
-    private Bitmap getCroppedBitmap(Bitmap bmp, int radius) {
-        Bitmap sbmp;
-
-        if (bmp.getWidth() != radius || bmp.getHeight() != radius) {
-            float smallest = Math.min(bmp.getWidth(), bmp.getHeight());
-            float factor = smallest / radius;
-            sbmp = Bitmap.createScaledBitmap(bmp,
-                    (int) (bmp.getWidth() / factor),
-                    (int) (bmp.getHeight() / factor), false);
-        } else {
-            sbmp = bmp;
-        }
-
-        Bitmap output = Bitmap.createBitmap(radius, radius, Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(output);
-
-        final String color = "#BAB399";
-        final Paint paint = new Paint();
-        final Rect rect = new Rect(0, 0, radius, radius);
-
-        paint.setAntiAlias(true);
-        paint.setFilterBitmap(true);
-        paint.setDither(true);
-        canvas.drawARGB(0, 0, 0, 0);
-        paint.setColor(Color.parseColor(color));
-        canvas.drawCircle(radius / 2 + 0.7f, radius / 2 + 0.7f,
-                radius / 2 + 0.1f, paint);
-        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-        canvas.drawBitmap(sbmp, rect, rect, paint);
-
-        return output;
-    }
 }
